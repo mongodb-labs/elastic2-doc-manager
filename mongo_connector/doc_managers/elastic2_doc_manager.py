fa853e8d7d0367259215f04bdea5bbd03ad1144d
Oz Bar Shalom <oz.barshalom@gmail.com>
2017-03-31 00:18:21 +0300

    fix indents on wrap_exceptions

--- mongo_connector/doc_managers/elastic2_doc_manager.py
+++ mongo_connector/doc_managers/elastic2_doc_manager.py
@@ -61,8 +61,8 @@ wrap_exceptions = exception_wrapper({
     es_exceptions.TransportError: errors.OperationFailed,
     es_exceptions.NotFoundError: errors.OperationFailed,
     es_exceptions.RequestError: errors.OperationFailed,
-	es_exceptions.ConnectionTimeout: errors.OperationFailed
-	})
+    es_exceptions.ConnectionTimeout: errors.OperationFailed
+    })
 
 LOG = logging.getLogger(__name__)
 
